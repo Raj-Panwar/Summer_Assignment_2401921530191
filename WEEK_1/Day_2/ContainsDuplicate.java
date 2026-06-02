23b396296142f79bb37ac2d34fd29f4f89b2a5c1
Raj-Panwar <raj.panwar1608@gmail.com>
2026-06-02 15:37:05 +0530

    Added Week 1 Day 2

---
+++ WEEK_1/Day_2/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for(int i : nums){
+            if(s.add(i)==false){
+                return true;
+            }
+            
+        }
+        return false;
+       
+        
+    }
+}
