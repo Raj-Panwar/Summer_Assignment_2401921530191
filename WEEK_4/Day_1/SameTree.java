093210d3386f9f6f42261a91f9c94f6bcb43620f
Raj-Panwar <raj.panwar1608@gmail.com>
2026-06-22 11:29:05 +0530

    Week 4 Day 1 and OOPs

---
+++ WEEK_4/Day_1/SameTree.java
@@ -0,0 +1,14 @@
+package WEEK_4.Day_1;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        
+        if(p.val!=q.val) return false;
+        else{
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        
+    }
+}
